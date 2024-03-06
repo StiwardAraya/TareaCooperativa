c338c0fa19c12347dedf35e400369b79b95b8cb3
Stiward Araya <StiwardArayaCalderon98@gmail.com>
2024-03-05 21:02:44 -0600

    Primeros archivos agregados

---
+++ src/main/java/cr/ac/una/tareacooperativa/controller/LoginController.java
@@ -0,0 +1,25 @@
+package cr.ac.una.tareacooperativa.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * Universidad Nacional - Programación II 2024
+ * Controlador de la pantalla de inicio de sesión
+ * @author Stiward Araya 
+ * @author Justin Mendez
+ */
+public class LoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
