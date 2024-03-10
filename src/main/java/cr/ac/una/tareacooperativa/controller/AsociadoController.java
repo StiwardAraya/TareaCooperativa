836cb067a5ce6ec93d3e9f84979e2c7e8624881e
Stiward Araya <StiwardArayaCalderon98@gmail.com>
2024-03-09 23:22:35 -0600

    FXML vacíos agregados, controladores base agregados, fxml enlazados a la hoja de css

---
+++ src/main/java/cr/ac/una/tareacooperativa/controller/AsociadoController.java
@@ -0,0 +1,23 @@
+package cr.ac.una.tareacooperativa.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * Universidad Nacional - Programación II 2024
+ * Controlador de la pantalla de asociados
+ * @author Stiward Araya
+ * @author Justin Mendez
+ */
+public class AsociadoController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {}    
+    
+}
---
+++ src/main/java/cr/ac/una/tareacooperativa/controller/ProfesorController.java
@@ -0,0 +1,23 @@
+package cr.ac.una.tareacooperativa.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * Universidad Nacional - Programación II 2024
+ * Controlador de la pantalla de profesores
+ * @author Stiward Araya
+ * @author Justin Mendez
+ */
+public class ProfesorController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {}    
+    
+}
