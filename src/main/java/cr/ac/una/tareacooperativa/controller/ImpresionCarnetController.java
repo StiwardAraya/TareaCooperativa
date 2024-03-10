ef8a9b4c842f96ba59d201939efe715cc669044c
Stiward Araya <StiwardArayaCalderon98@gmail.com>
2024-03-09 21:49:38 -0600

    Css de impresion carnet estandarizado, controlador de impresion carnet agregado

---
+++ src/main/java/cr/ac/una/tareacooperativa/controller/ImpresionCarnetController.java
@@ -0,0 +1,39 @@
+package cr.ac.una.tareacooperativa.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * Universidad Nacional - Programación II 2024
+ * Controlador de la pantalla de impresion de carnet
+ * @author Stiward Araya
+ * @author Justin Mendez
+ */
+public class ImpresionCarnetController implements Initializable{
+
+    @javafx.fxml.FXML
+    private TextField txtNombreAsociado;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {}
+
+    @javafx.fxml.FXML
+    public void imprimirCarnet(Event event) {
+        //TODO accion para imprimir el carnet
+    }
+
+    @javafx.fxml.FXML
+    public void volver(Event event) {
+        //TODO accion para regresar
+    }
+
+}
