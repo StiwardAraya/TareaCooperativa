56a792f4701fe19773dfdc287b74371e169743b2
Stiward Araya <StiwardArayaCalderon98@gmail.com>
2024-03-11 22:25:38 -0600

    Flujo del botón de profesores a la ventana de profesores agregado

--- src/main/java/cr/ac/una/tareacooperativa/controller/LoginController.java
+++ src/main/java/cr/ac/una/tareacooperativa/controller/LoginController.java
@@ -1,7 +1,10 @@
 package cr.ac.una.tareacooperativa.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import cr.ac.una.tareacooperativa.App;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.Initializable;
@@ -27,22 +30,22 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {}
 
     @javafx.fxml.FXML
-    public void abrirWeb(Event event) {
+    public void abrirWeb(Event event) throws IOException{
         //TODO abrir webView con la web
     }
 
     @javafx.fxml.FXML
-    public void abrirVentanaFuncionarios(ActionEvent actionEvent) {
+    public void abrirVentanaFuncionarios(ActionEvent actionEvent) throws IOException {
         //TODO abrir ventana de funcionarios
     }
 
     @javafx.fxml.FXML
-    public void abrirVentanaProfesores(ActionEvent actionEvent) {
-        //TODO abrir ventana de profesores
+    public void abrirVentanaProfesores(ActionEvent actionEvent) throws IOException {
+        App.setRoot("profesorView");
     }
 
     @javafx.fxml.FXML
-    public void abrirVentanaAsociados(ActionEvent actionEvent) {
+    public void abrirVentanaAsociados(ActionEvent actionEvent) throws IOException {
         //TODO abrir ventana de asociados
     }
 }
