eb273e5181ccdd9b886a93503e6b253ca716ac26
JustinMdz <justin.mendez.mena@est.una.ac.cr>
2024-03-15 21:29:21 -0600

    Drag and drop of logo image added in the maintenance window

--- src/main/java/cr/ac/una/tareacooperativa/controller/MantenimientoCoopeController.java
+++ src/main/java/cr/ac/una/tareacooperativa/controller/MantenimientoCoopeController.java
@@ -1,11 +1,18 @@
 package cr.ac.una.tareacooperativa.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 
 /**
  * Universidad Nacional - Programación II 2024
@@ -17,7 +24,7 @@ import javafx.scene.control.TextField;
 public class MantenimientoCoopeController implements Initializable {
 
     @FXML
-    private TextField txtNombreCoope;
+    private ImageView imgViewLogo;
 
     /**
      * Initializes the controller class.
@@ -28,13 +35,28 @@ public class MantenimientoCoopeController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) { }    
 
     @FXML
-    private void volver(ActionEvent event) {
-        //TODO regresar a la ventana anterior
+    private void imgViewLogoOnDragOver(DragEvent event) {
+         Dragboard dragboard = event.getDragboard();
+     if(dragboard.hasImage()||dragboard.hasFiles()){
+     event.acceptTransferModes(TransferMode.COPY);
+     }
+         System.out.println("DragOver");
     }
-
+    
     @FXML
-    private void guardarCambios(ActionEvent event) {
-        //TODO guardar los cambios
+    private void imgViewLogoOnDragDropped(DragEvent event) {
+         Dragboard dragboard = event.getDragboard();
+     if(dragboard.hasImage()||dragboard.hasFiles()){
+               try
+               {
+                   imgViewLogo.setImage(new Image(new FileInputStream(dragboard.getFiles().get(0))));
+               } catch (FileNotFoundException ex)
+               {
+                   ex.printStackTrace();
+               }
+     }
+        System.out.println("Dropped");
     }
+    
     
 }
